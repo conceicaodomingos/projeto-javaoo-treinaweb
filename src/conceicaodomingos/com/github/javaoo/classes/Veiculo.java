47efcaddb25ee84567a8f79a186ff2e0da8bbc81
conceicaodomingos <conceicaojpdomingos@gmail.com>
2026-06-22 21:10:51 +0100

    first commit

---
+++ src/conceicaodomingos/com/github/javaoo/classes/Veiculo.java
@@ -0,0 +1,17 @@
+package conceicaodomingos.com.github.javaoo.classes;
+
+public class Veiculo {
+
+	public String nome;
+	public String marca;
+	public String chassi;
+	public String quantidadeRodas;
+	
+	public void ligar() {
+		System.out.println("O veículo ligou!");
+	}
+	
+	public void desligar() {
+		System.out.println("O veículo desligou!");
+	}
+}
